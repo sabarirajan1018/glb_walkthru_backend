0210917392189b5f9d45f6c1d85e05fb8b85f15b
sabarirajan1018 <sabarirajan@clariscosolutions.com>
2026-05-14 14:47:56 +0530

    remove cors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
 const modelRoutes = require('./routes/modelRoutes');
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:3000' }));
+// app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:3000' }));
 app.use(express.json());
 
 // Serve static GLB files
